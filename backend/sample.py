d8b70412c7e6916585277f880bf45611ba852603
James Ball <james@ball.sh>
2021-06-30 18:28:11 +0100

    Implement file download of samples

--- backend/sample.py
+++ backend/sample.py
@@ -1,12 +1,36 @@
+import os
 import time
 from datetime import datetime
+from tempfile import NamedTemporaryFile
 
-from flask import Blueprint, request, Response, abort
+from flask import Blueprint, request, Response, abort, send_file
 from flask_login import current_user, login_required
 
 from backend.model import db, Member, Sample
 
 sample = Blueprint('sample', __name__)
+
+download_map = {}
+
+
+@sample.route('/sample/<int:sample_id>/download')
+@login_required
+def download_sample(sample_id):
+    sample = Sample.query.get_or_404(sample_id)
+    file_ext = sample.name.rsplit('.', 1)[1].lower()
+    temp = NamedTemporaryFile(suffix="." + file_ext, delete=False)
+    temp.write(sample.audio_data)
+    temp.close()
+
+    if sample_id not in download_map:
+        download_map[sample_id] = []
+
+    if current_user.id not in download_map[sample_id]:
+        sample.num_downloads = Sample.num_downloads + 1
+        download_map[sample_id].append(current_user.id)
+        db.session.commit()
+
+    return send_file(temp.name)
 
 
 def get_samples():
