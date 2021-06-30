cfa515a8586e66944b483d44ea5eed83f051e78d
James Ball <james@ball.sh>
2021-06-30 14:14:59 +0100

    Implement getting samples from database

--- backend/sample.py
+++ backend/sample.py
@@ -8,3 +8,30 @@ from backend.model import db, Member, Sample
 
 sample = Blueprint('sample', __name__)
 
+
+@sample.route('/samples', methods=['GET'])
+@login_required
+def get_samples():
+    samples = Sample.query.all()
+
+    samples.sort(key=lambda s: s.num_downloads)
+
+    samples_obj = []
+    for sample in samples:
+        creator = Member.query.get(sample.creator)
+
+        samples_obj.append({
+            'id': sample.id,
+            'name': sample.name,
+            'description': sample.description,
+            'sample_rate': sample.sample_rate,
+            'bit_depth': sample.bit_depth,
+            'num_downloads': sample.num_downloads,
+            'first_name': creator.first_name,
+            'last_name': creator.last_name,
+            'username': creator.username,
+        })
+
+    return {
+        'samples': samples_obj,
+    }
